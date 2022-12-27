336621d3a5d2de0bf579907e27758e40301bddfe
Vladimir2k22 <121224642+Vladimir2k22@users.noreply.github.com>
2022-12-27 18:35:44 +0800

    Update main2.py

--- december-27/main2.py
+++ december-27/main2.py
@@ -1,16 +1,16 @@
 g = input("введите красный цвет:")
 t = input("введите желтый цвет:")
-if g == red and t == red: 
+if g == "red" and t == "red": 
     print("red")
-if g == blue and t == blue:
+if g == "blue" and t == "blue":
     print("blue")
-if g == yelow and t == yelow:
+if g == "yelow" and t == "yelow":
     print("yelow")
-if g == red and t == blue:
+if g == "red" and t == "blue":
     print("purple")
-if g == red and t == yelow:
+if g == "red" and t == "yelow":
     print("orange")
-if g == blue and t == yelow: 
+if g == "blue" and t == "yelow": 
     print("green")
 
 
