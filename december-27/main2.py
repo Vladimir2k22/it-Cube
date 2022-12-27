4f9b102e6eb7f2bf512abc60a491fc2993576ce4
Vladimir2k22 <121224642+Vladimir2k22@users.noreply.github.com>
2022-12-27 18:33:00 +0800

    Add files via upload

---
+++ december-27/main2.py
@@ -0,0 +1,16 @@
+g = input("введите красный цвет:")
+t = input("введите желтый цвет:")
+if g == red and t == red: 
+    print("red")
+if g == blue and t == blue:
+    print("blue")
+if g == yelow and t == yelow:
+    print("yelow")
+if g == red and t == blue:
+    print("purple")
+if g == red and t == yelow:
+    print("orange")
+if g == blue and t == yelow: 
+    print("green")
+
+
